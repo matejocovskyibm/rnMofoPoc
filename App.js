e23756e32a4536a8b1e737ea5f936d474c95540e
Matej Ocovsky <matej@matejs-mbp.home>
2020-03-02 16:40:49 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2018 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Button, flex} from 'react-native';
+import {WLAuthorizationManager, WLResourceRequest } from 'react-native-ibm-mobilefirst'
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  pingMFP() {
+    WLAuthorizationManager.obtainAccessToken("").then(
+      (token) => {
+        console.log('-->  pingMFP(): Success ', token);
+        var resourceRequest = new WLResourceRequest("/adapters/NodeAdapter/getCurrentTime",
+          WLResourceRequest.GET
+        );
+        // resourceRequest.setQueryParameters({ name: "world" });
+        resourceRequest.send().then(
+          (response) => {
+            // Will display "Hello world" in an alert dialog.
+            var newJson = JSON.parse(response.responseText);
+            alert("Success: " + newJson.time);
+          },
+          (error) => {
+            console.error(error);
+            alert("Failure: Resource Request");
+          }
+        );
+      }, (error) => {
+        console.error(error);
+        alert("Failed to connect to MobileFirst Server");
+      });
+  }
+  render() {
+    return (
+      <View style={styles.container} >
+        <Text style={styles.welcome}>Hello MobileFirst</Text>
+        <View style={styles.buttonContainer}>
+          <Button title="Ping MFP server" color="white" onPress={this.pingMFP} accessibilityLabel="Press this to ping MFP Server"></Button>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent:'space-between',
+    alignItems:"center",
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    
+  },
+  buttonContainer:{
+    
+  }
+});
---
+++ README.md
@@ -0,0 +1,37 @@
+IBM MobileFirst Platform Foundation
+===
+## MFP Starter React Native
+A sample React Native application with MobileFirst Capability which does basic OAuth communication with MobileFirst Server.
+
+### Usage
+
+1. From the command-line window, navigate to the project's roots folder and run the commands:
+ - `npm install react-native-ibm-mobilefirst` - to install Mobilefirst react native SDK.
+ - `npm install` - to install project dependencies.
+ - `react-native link` - to link libraries to respective platforms based on dependencies.
+
+2. From a command-line window, navigate to the platforms's root folder (either iOS or Android)  and run the commands:
+ - `mfpdev app register` - to register the application.
+ 
+3. From the command-line window, navigate to the project's roots folder and run the command:
+ - `react-native run-ios` or `react-native run-android` - to run the application in an iOS Emulator or Android Simulator.
+
+4. Press the **Ping MobileFirst Server** button to ping MobileFirst Server.
+
+### Supported Levels
+IBM MobileFirst Platform Foundation 8.0
+
+### License
+Copyright 2018 IBM Corp.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+att
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
